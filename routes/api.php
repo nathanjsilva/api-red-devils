9c714946e0717d33aefa04f5bebde86df9e04575
Nathan Silva <nathan.ads.100@gmail.com>
2025-04-16 22:28:25 -0300

    criando rotas

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,30 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlayerController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PeladaController;
+use App\Http\Controllers\MatchPlayerController;
 
-Route::get('/', function () {
-    return response()->json(['message' => 'API Red Devils - Laravel 11']);
-});
-Route::prefix('players')->group(function () {
-    Route::post('/', [PlayerController::class, 'store']);        
-    Route::get('/', [PlayerController::class, 'index']);         
-    Route::get('/{id}', [PlayerController::class, 'show']);      
-    Route::put('/{id}', [PlayerController::class, 'update']);    
-    Route::delete('/{id}', [PlayerController::class, 'destroy']); 
-});
+// Rota pública de login
 Route::post('/login', [AuthController::class, 'login']);
-Route::apiResource('peladas', PeladaController::class);
+
+// Grupo protegido com autenticação Sanctum
+Route::middleware('auth:sanctum')->group(function () {
+    
+    // Rotas de jogadores
+    Route::get('/players', [PlayerController::class, 'index']);
+    Route::get('/players/{id}', [PlayerController::class, 'show']);
+    Route::post('/players', [PlayerController::class, 'store']);
+    Route::put('/players/{id}', [PlayerController::class, 'update']);
+    Route::delete('/players/{id}', [PlayerController::class, 'destroy']);
+
+    // Rotas de peladas
+    Route::get('/peladas', [PeladaController::class, 'index']);
+    Route::get('/peladas/{id}', [PeladaController::class, 'show']);
+    Route::post('/peladas', [PeladaController::class, 'store']);
+    Route::put('/peladas/{id}', [PeladaController::class, 'update']);
+    Route::delete('/peladas/{id}', [PeladaController::class, 'destroy']);
+
+    // Rotas para registrar estatísticas de jogadores nas peladas
+    Route::post('/match-players', [MatchPlayerController::class, 'store']);
+    Route::put('/match-players/{id}', [MatchPlayerController::class, 'update']);
+    Route::delete('/match-players/{id}', [MatchPlayerController::class, 'destroy']);
+});
